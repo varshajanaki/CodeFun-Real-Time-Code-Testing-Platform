c78a0b4a6d5faf9b6fbc4de65d9a5d61f10e363c
varshajanaki <156770739+varshajanaki@users.noreply.github.com>
2024-09-01 17:55:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function run() {
+function run()
+{
     let htmlcode = document.getElementById("html-code").value;
     let csscode = document.getElementById("css-code").value;
     let jscode = document.getElementById("js-code").value;
-    let output = document.getElementById("output");
-    output.contentDocument.body.innerHTML = htmlcode + "<style>" + csscode + "</style>" + "<scri" + "pt>" + jscode + "</scri" + "pt>";;
+    // let output = document.getElementById("output");
+  //  output.contentDocument.body.innerHTML = htmlcode + "<style>" + csscode + "</style>" + "<scri" + "pt>" + jscode + "</scri" + "pt>";; 
+
+    //my code 
+     // Clear the iframe content before writing new content
+    let doc = output.contentDocument || output.contentWindow.document;
+    doc.open();
+    doc.write('<!DOCTYPE html><html><head><style>' + csscode + '</style></head><body>' + htmlcode + '<script>' + jscode + '<\/script></body></html>');
+    doc.close();
 
 }
